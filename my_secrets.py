a9a1433dc69ea626c76f7e8f3db26ff315d12cc1
Kevin <kevinksingh05@gmail.com>
2023-01-11 11:39:29 +0000

    Update my_secrets.py
    updated file to remove my personal accounts incase someone else wants to use this

--- my_secrets.py
+++ my_secrets.py
@@ -2,5 +2,5 @@ def get_api_key():
     return 0
 
 def get_account_name():
-    account_list = ['Terrible Toucan', 'singhasong', 'Stroblitz']
+    account_list = ['my_account_name']
     return account_list
