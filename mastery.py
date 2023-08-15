cc46433f80c49edeb15ff1077272065192eb66b0
Kevin <kevinksingh05@gmail.com>
2023-08-15 12:21:32 +0100

    updated with a function
    new function estimates the number of games played using a predefined winrate and assuming you gain 1000 mastery per win and 100 mastery per loss

--- mastery.py
+++ mastery.py
@@ -17,11 +17,15 @@ def get_mastery_points(api_key, region, summoner_name, champion_id):
         print(f"Error: {e.response.status_code} - {e.response.text}")
         return None
 
+def estimate_games_played(master_points, win_rate):
+    return master_points/(900.0*win_rate + 100.0)
+
 # Replace with your Riot API key
 riot_api_key = get_api_key()
 
 # Replace with the region, summoner name, and champion ID you want to use
 region = "euw1"
+#summoner_name = ["Singhasong", "tsuki no kokyuu", "Stroblitz"]
 summoner_name = []
 # This is the champion ID for Yasuo
 champion_id = 157
@@ -30,5 +34,7 @@ for summoner in summoner_name:
     mastery_points = get_mastery_points(riot_api_key, region, summoner, champion_id)
     if mastery_points is not None:
         total += mastery_points 
-print(f"The total mastery points for Yasuo are: {total}")
 
+games = estimate_games_played(total, 0.70)
+print(f"The total mastery points for Yasuo are: {total}")
+print(f"The total estimated games for Yasuo are: {games}")
