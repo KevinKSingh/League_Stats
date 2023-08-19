0003622bd8895f3f35fb4e4ed3ea1d8ab26ee33b
Kevin <kevinksingh05@gmail.com>
2023-08-19 14:20:55 +0100

    Update my_secrets.py

--- my_secrets.py
+++ my_secrets.py
@@ -4,3 +4,15 @@ def get_api_key():
 def get_account_name():
     account_list = ['my_account_name']
     return account_list
+
+def get_my_accounts():
+    account_list = ['']
+    return account_list 
+
+def get_other_account():
+    account_list = ['']
+    return account_list 
+
+def get_korea_accounts():
+    account_list = ['']
+    return account_list 
